d5a25397b7882467883ef19521f43d68dee1d180
lawrence <svzef@outlook.com>
2018-01-09 17:25:19 +0800

    fake game

---
+++ main.py
@@ -0,0 +1,21 @@
+class kalusfighter(object):
+	def __init__(Person, ATK, HP, MP, name, job):
+		Person.HP = HP
+		Person.MP = MP
+		Person.name = name
+		Person.job = job
+		Person.ATK = ATK
+
+	def attack(Person, afterattack):
+		afterattack.HP -= Person.ATK
+
+		while afterattack.HP <= 0
+		print ('{} dead').format(afterattack.name)
+		or message = '{} has {} point remaining'.format(afterattack.name, afterattack.HP)
+		print (message)
+
+alex = kalusfighter(ATK=20, HP=100, MP=50, name='alex', job='saber')
+bob = kalusfighter(ATK=30, HP=200, MP=200, name='bob', job='archer')
+
+print ('{} is a {}, ATK:{}, HP:{}, MP:{} '.format(alex.name, alex.job, alex.ATK, alex.HP, alex.MP))
+print ('{} is a {}, ATK:{}, HP:{}, MP:{} '.format(bob.name, bob.job, bob.ATK, bob.HP, bob.MP))
